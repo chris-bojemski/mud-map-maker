7fbf4081450a10f75862852f38d56e3b69f868e1
Chris Bojemski <chris.bojemski@gmail.com>
2018-12-19 00:27:40 -0500

    Make description respond to change.

--- src/components/DetailPanel.js
+++ src/components/DetailPanel.js
@@ -3,15 +3,47 @@ import React from 'react'
 import './styles/DetailPanel.css'
 
 class DetailPanel extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      description: this.props.clickedDescription
+    }
+  }
+
+  changeDescription = event => {
+    this.setState({ description: event.target.value })
+  }
+
+  getDetailsFromProps = () => {
+    if (this.props.clicked) {
+      return (
+        <>
+          <h3>{this.props.clicked}</h3>
+          <label htmlFor="description">
+            Description:
+          </label>
+          <br></br>
+          <input
+            type="textarea"
+            value={this.state.description}
+            onChange={this.changeDescription}
+            name="description"
+          ></input>
+        </>
+      )
+    } else {
+      return (
+        <h3>Nothing selected.</h3>
+      )
+    }
+  }
+
   render() {
     return (
       <div className={`detail-panel`}>
         <h2>Detail</h2>
-        <h3>{this.props.clicked}</h3>
-        <input
-          type="textarea"
-          value={this.props.clickedDescription}
-        ></input>
+        <hr></hr>
+        {this.getDetailsFromProps()}
       </div>
     )
   }
