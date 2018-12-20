4c4d3ea94a79335b9476e1793177a687eb136057
Chris Bojemski <chris.bojemski@gmail.com>
2018-12-19 20:21:01 -0500

    Surround grid controls in their own div.

--- src/components/GridForm.js
+++ src/components/GridForm.js
@@ -23,53 +23,55 @@ class GridForm extends React.Component {
     return (
       <div className="grid-form-container">
         <form>
-          <label htmlFor="gridRows">Rows: </label>
-          <input 
-            disabled={this.state.locked}
-            type="number" 
-            name="gridRows" 
-            placeholder="2, 3, 4..." 
-            value={this.props.gridRows}
-            onChange={(event) => this.props.changeRows(event.target.value)}
-          />
+          <div className="grid-controls">
+            <label htmlFor="gridRows">Rows: </label>
+            <input 
+              disabled={this.state.locked}
+              type="number" 
+              name="gridRows" 
+              placeholder="2, 3, 4..." 
+              value={this.props.gridRows}
+              onChange={(event) => this.props.changeRows(event.target.value)}
+            />
 
-          <label htmlFor="gridCols">Cols: </label>
-          <input 
-            disabled={this.state.locked}
-            type="number" 
-            name="gridCols" 
-            placeholder="2, 3, 4..." 
-            value={this.props.gridCols}
-            onChange={(event) => this.props.changeCols(event.target.value)}
-          />
+            <label htmlFor="gridCols">Cols: </label>
+            <input 
+              disabled={this.state.locked}
+              type="number" 
+              name="gridCols" 
+              placeholder="2, 3, 4..." 
+              value={this.props.gridCols}
+              onChange={(event) => this.props.changeCols(event.target.value)}
+            />
 
-          <label htmlFor="lock">Lock</label>
-          <input 
-            type="checkbox"
-            onChange={this.toggleLock}
-            name="lock"
-          />
+            <label htmlFor="lock">Lock</label>
+            <input 
+              type="checkbox"
+              onChange={this.toggleLock}
+              name="lock"
+            />
 
-          <label 
-            htmlFor="grid-scale"
-          >
-            Zoom ({this.props.gridScale}): 
-          </label>
-          <input 
-            type="range"
-            name="grid-scale"
-            min="25"
-            max="200"
-            defaultValue={this.props.gridScale}
-            onChange={(event) => this.props.updateGridScale(event.target.value)}
-          >
-          </input>
+            <label 
+              htmlFor="grid-scale"
+            >
+              Zoom ({this.props.gridScale}): 
+            </label>
+            <input 
+              type="range"
+              name="grid-scale"
+              min="25"
+              max="200"
+              defaultValue={this.props.gridScale}
+              onChange={(event) => this.props.updateGridScale(event.target.value)}
+            >
+            </input>
 
-          <button
-            onClick={(event) => this.handleDeselect(event)}
-          >
-            Deselect Room(s)
-          </button>
+            <button
+              onClick={(event) => this.handleDeselect(event)}
+            >
+              Deselect Room(s)
+            </button>
+          </div>
         </form>
       </div>
     )
